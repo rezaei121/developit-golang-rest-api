c9d6e72c66d181e618dc608ca122aea462fb34a6
rezaei121 <mr.rezaee121@gmail.com>
2019-10-18 00:21:14 +0330

    goswagger

--- main.go
+++ main.go
@@ -26,34 +26,13 @@ func main() {
 	router.POST("/v1/user/register", userController.ActionRegister)
 	router.POST("/v1/dashboard/user/profile", middlewares.TokenAuth(userController.ActionProfile, connection))
 	scanDir()
-	router.GET("/api/api-doc.json", apiDoc)
+	router.GET("/api/swagger.yml", apiDoc)
 
 	http.ListenAndServe(":8080", router)
 }
 
-func scanDir() {
-	var data string
-	err := filepath.Walk("./api",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			if !info.IsDir() && filepath.Ext(info.Name()) == ".swg" {
-				//fmt.Println(path, info.Size(), filepath.Ext(info.Name()))
-				swgFile, _ := ioutil.ReadFile(path)
-				data = data + string(swgFile) + "\n"
-			}
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
-
-	ioutil.WriteFile("./api/api-doc.json", []byte(data), 0644)
-}
-
 func apiDoc(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	data, _ := ioutil.ReadFile("./api/api-doc.json")
+	data, _ := ioutil.ReadFile("./swagger.yml")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintf(w, string(data))
 }
