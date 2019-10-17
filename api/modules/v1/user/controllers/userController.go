1bfbe1ff395877c0d2aa548b425133e737fde3be
rezaei121 <mr.rezaee121@gmail.com>
2019-10-17 21:36:10 +0330

    add swagger

---
+++ api/modules/v1/user/controllers/userController.go
@@ -0,0 +1,96 @@
+package controllers
+
+import (
+	models2 "developit-golang-rest-api/api/modules/v1/user/models"
+	"developit-golang-rest-api/components/helpers/httperror"
+	"developit-golang-rest-api/components/helpers/password"
+	"developit-golang-rest-api/components/helpers/randomstring"
+	"encoding/json"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/asaskevich/govalidator.v4"
+	"net/http"
+	"time"
+)
+
+type UserController struct {
+	db *gorm.DB
+}
+
+func NewUserController(db *gorm.DB) *UserController {
+	return &UserController{db}
+}
+
+func IndexAction(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "user index action...")
+}
+
+func (controller UserController) ActionProfile(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	httperror.New(404, "coming soon...", rw)
+	return
+}
+
+func (controller UserController) ActionLogin(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	decoder := json.NewDecoder(r.Body)
+	var input models2.UserLogin
+	decoder.Decode(&input)
+	user := &models2.User{}
+	controller.db.Where("email = ?", input.Email).Find(user)
+
+	if user.Email == "" {
+		httperror.New(http.StatusNotFound, "username or password is incorrect!", rw)
+		return
+	}
+
+	if password.CheckHash(input.Password+user.Sult, user.Password) {
+		userTokenModel := models2.UserToken{
+			UserId:    user.Id,
+			Token:     randomstring.New(32),
+			CreatedAt: time.Time{},
+		}
+		controller.db.Create(&userTokenModel)
+		result, _ := json.Marshal(userTokenModel)
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(result)
+	} else {
+		httperror.New(http.StatusBadRequest, "username or password is incorrect!", rw)
+		return
+	}
+}
+
+func (controller UserController) ActionRegister(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	decoder := json.NewDecoder(r.Body)
+	var input models2.User
+	decoder.Decode(&input)
+
+	sultString := randomstring.New(8)
+	passwordHash := password.Hash(input.Password + sultString)
+
+	userModel := models2.User{
+		Name:      input.Name,
+		Surname:   input.Surname,
+		Email:     input.Email,
+		Password:  passwordHash,
+		Status:    input.Status,
+		Sult:      sultString,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	}
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	_, validateError := govalidator.ValidateStruct(&userModel)
+	if validateError != nil {
+		httperror.New(http.StatusBadRequest, validateError.Error(), rw)
+		return
+	}
+
+	result := controller.db.Create(&userModel)
+	if len(result.GetErrors()) > 0 {
+		httperror.New(http.StatusBadRequest, "can not create", rw)
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+}
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"developit-golang-rest-api/db"
-	"developit-golang-rest-api/middlewares"
-	"developit-golang-rest-api/modules/v1/user/controllers"
+	"developit-golang-rest-api/api/modules/v1/user/controllers"
+	"developit-golang-rest-api/components/db"
+	"developit-golang-rest-api/components/middlewares"
+	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -20,6 +25,35 @@ func main() {
 	router.POST("/v1/user/login", userController.ActionLogin)
 	router.POST("/v1/user/register", userController.ActionRegister)
 	router.POST("/v1/dashboard/user/profile", middlewares.TokenAuth(userController.ActionProfile, connection))
+	scanDir()
+	router.GET("/api/api-doc.json", apiDoc)
 
 	http.ListenAndServe(":8080", router)
+}
+
+func scanDir() {
+	var data string
+	err := filepath.Walk("./api",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() && filepath.Ext(info.Name()) == ".swg" {
+				//fmt.Println(path, info.Size(), filepath.Ext(info.Name()))
+				swgFile, _ := ioutil.ReadFile(path)
+				data = data + string(swgFile) + "\n"
+			}
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+
+	ioutil.WriteFile("./api/api-doc.json", []byte(data), 0644)
+}
+
+func apiDoc(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	data, _ := ioutil.ReadFile("./api/api-doc.json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, string(data))
 }
