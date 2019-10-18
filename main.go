5ffaf5dd4dce88a34035132d05e4d1a73a509206
rezaei121 <mr.rezaee121@gmail.com>
2019-10-18 22:48:25 +0330

    add swg ui & swg editor

--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"developit-golang-rest-api/api/modules/v1/user/controllers"
 	"developit-golang-rest-api/components/db"
 	"developit-golang-rest-api/components/middlewares"
-	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/julienschmidt/httprouter"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -25,14 +20,6 @@ func main() {
 	router.POST("/v1/user/login", userController.ActionLogin)
 	router.POST("/v1/user/register", userController.ActionRegister)
 	router.POST("/v1/dashboard/user/profile", middlewares.TokenAuth(userController.ActionProfile, connection))
-	scanDir()
-	router.GET("/api/swagger.yml", apiDoc)
-
+	router.ServeFiles("/api/swagger/*filepath", http.Dir("api/swagger"))
 	http.ListenAndServe(":8080", router)
-}
-
-func apiDoc(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	data, _ := ioutil.ReadFile("./swagger.yml")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, string(data))
 }
