719d3b841edfc879c70ade54d9be364a1eccb2b3
rezaei121 <mr.rezaee121@gmail.com>
2019-10-19 00:53:37 +0330

    swagger

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"developit-golang-rest-api/api/modules/v1/user/controllers"
 	"developit-golang-rest-api/components/db"
 	"developit-golang-rest-api/components/middlewares"
+	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/julienschmidt/httprouter"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -20,6 +23,31 @@ func main() {
 	router.POST("/v1/user/login", userController.ActionLogin)
 	router.POST("/v1/user/register", userController.ActionRegister)
 	router.POST("/v1/dashboard/user/profile", middlewares.TokenAuth(userController.ActionProfile, connection))
-	router.ServeFiles("/api/swagger/*filepath", http.Dir("api/swagger"))
+
+	router.GET("/api/swagger/api-doc", ActionApiDoc)
+	router.GET("/api/swagger/schemas/:name", ActionApiSchemas)
+
+	//router.ServeFiles("/api/swagger/*filepath", http.Dir("api/swagger"))
+
 	http.ListenAndServe(":8080", router)
+}
+
+func ActionApiDoc(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	file, error := ioutil.ReadFile("./api/swagger/api-doc.yaml")
+	if error != nil {
+		log.Fatal(error)
+	}
+	rw.Header().Set("Access-Control-Allow-Methods", r.Header.Get("Allow"))
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprint(rw, string(file))
+}
+
+func ActionApiSchemas(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	file, error := ioutil.ReadFile("./api/swagger/schemas/" + p.ByName("name") + ".yaml")
+	if error != nil {
+		log.Fatal(error)
+	}
+	rw.Header().Set("Access-Control-Allow-Methods", r.Header.Get("Allow"))
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprint(rw, string(file))
 }
